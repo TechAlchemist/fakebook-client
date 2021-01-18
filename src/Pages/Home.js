0e4bb9b06b0dd5616d47bbd8e6c3cd8e18ae5495
Brandon Myers <brandonRmyers1512@gmail.com>
2021-01-18 13:50:15 -0800

    Adding user registration.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,25 +9,7 @@ import '../App.css';
 
 function Home() {
 
-    const { loading, data: {getPosts: posts} } = useQuery(FETCH_POSTS_QUERY);
-
-    return (
-        <Grid columns={1} >
-            <Grid.Row className="page-title">
-                <h1> Recent Posts</h1>
-            </Grid.Row>
-            <Grid.Row>
-                {loading ? (<h1>loading posts..</h1>) : ( posts && posts.map(post => (
-                    <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                        <PostCard post={post} />
-                    </Grid.Column>
-                )) )}
-            </Grid.Row>
-        </Grid>
-    )
-}
-
-const FETCH_POSTS_QUERY = gql`
+    const FETCH_POSTS_QUERY = gql`
     {
         getPosts {
             id body createdAt username
@@ -45,5 +27,26 @@ const FETCH_POSTS_QUERY = gql`
         }
     }
 `;
+
+    const { loading, error, data } = useQuery(FETCH_POSTS_QUERY);
+
+    console.log(data)
+    return (
+        <Grid columns={1} >
+            <Grid.Row className="page-title">
+                <h1> Recent Posts</h1>
+            </Grid.Row>
+            <Grid.Row>
+                {loading ? (<h1>loading posts..</h1>) : ( data && data.getPosts.map(post => (
+                    <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                        <PostCard post={post} />
+                    </Grid.Column>
+                )) )}
+            </Grid.Row>
+        </Grid>
+    )
+}
+
+
 
 export default Home;
