7c640a337dbce23c4b17de199b24c88e4f14ba5b
Brandon Myers <brandonRmyers1512@gmail.com>
2021-01-18 09:04:36 -0800

    Setting up menu and simple navigation.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import MenuBar from './Components/Menu';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Welcome to Fakebook. </h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Container>
+
+    </Router>
   );
 }
 
