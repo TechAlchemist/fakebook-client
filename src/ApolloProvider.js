45f64a12a663406b287577d088a501c6a0b4e66e
Brandon Myers <brandonRmyers1512@gmail.com>
2021-01-19 16:33:43 -0800

    Pointing apollo link to production server.

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,7 +6,7 @@ import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@ap
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://fakebookql-backend.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
